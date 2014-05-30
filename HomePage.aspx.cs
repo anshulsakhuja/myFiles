e5469cb776cd08aa612f18fca6a06252015f2ba8
anshulsakhuja <anshul.sakhuja13@gmail.com>
2014-05-30 17:51:38 +0530

    HomePage.aspx.cs

---
+++ HomePage.aspx.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class HomePage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Timer1_Tick(object sender, EventArgs e)
+        {
+            Random _rand = new Random();
+            int i = _rand.Next(0, 5);
+            Image1.ImageUrl = @"~/images/header_" + i.ToString() + ".jpg";
+        }
+    }
+}
